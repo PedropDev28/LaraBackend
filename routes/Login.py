e2c6cb9f31a301947d1b5ed415a13451d531744f
Pedro Rafael Lázaro Nevado <119618710+PedropDev28@users.noreply.github.com>
2024-11-20 18:50:52 +0000

    Actualizar respuesta del endpoint de inicio de sesión para incluir nombre e ID del usuario

--- routes/Login.py
+++ routes/Login.py
@@ -10,4 +10,4 @@ async def login(login_data: Login):
     usuario = await db["usuarios"].find_one({"mail": login_data.email, "password": login_data.password})
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
-    return {"email": usuario["mail"], "message": "Inicio de sesión exitoso"}
+    return {"email": usuario["mail"],"name": usuario["name"],"id": str(usuario["_id"])}
