1c16b120b8f449fe944dac83ea4255628154ba0f
Pedro Rafael Lázaro Nevado <119618710+PedropDev28@users.noreply.github.com>
2024-11-20 09:47:12 +0000

    Modificar la respuesta del endpoint de inicio de sesión para devolver 'mail' en lugar de 'email'

--- routes/Usuario.py
+++ routes/Usuario.py
@@ -56,4 +56,4 @@ async def login(login_data: LoginRequest):
     usuario = await db["usuarios"].find_one({"mail": login_data.email, "password": login_data.password})
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
-    return {"email": usuario["email"], "message": "Inicio de sesión exitoso"}
+    return {"email": usuario["mail"], "message": "Inicio de sesión exitoso"}
