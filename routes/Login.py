608196ae45b7bc96368e14b7d8fca2c00130f505
Pedro Rafael Lázaro Nevado <119618710+PedropDev28@users.noreply.github.com>
2024-11-20 19:07:26 +0000

    Actualizar respuesta del endpoint de inicio de sesión para incluir correo y mensaje de éxito

--- routes/Login.py
+++ routes/Login.py
@@ -10,4 +10,4 @@ async def login(login_data: Login):
     usuario = await db["usuarios"].find_one({"mail": login_data.email, "password": login_data.password})
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
-    return {"usuario": usuario}
+    return {"email": usuario["mail"], "nombre": usuario["nombre"], "message": "Inicio de sesión exitoso"}
