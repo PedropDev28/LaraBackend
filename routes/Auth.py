5468d9a709ca5562f9400c2a5e073ffad8c3d448
Pedro Rafael Lázaro Nevado <119618710+PedropDev28@users.noreply.github.com>
2024-11-21 18:50:02 +0000

    Modificar la forma de calcular la fecha de expiración del token de acceso para usar datetime.now en lugar de datetime.utcnow

--- routes/Auth.py
+++ routes/Auth.py
@@ -29,9 +29,9 @@ credentials_exception = HTTPException(
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(datetime.timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
